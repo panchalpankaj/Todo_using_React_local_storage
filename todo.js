60d7a20221059a8117803453cbadd0179b51d87f
Panchal Pankaj <panchalpankaj89658@gmail.com>
2024-01-19 19:15:15 +0530

    Todo List make using React and Use Local Storage

---
+++ todo.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import "./todo.css";
+
+const getLocalData = () => {
+  const lists = localStorage.getItem("mytodolist");
+
+  if (lists) {
+    return JSON.parse(lists);
+  } else {
+    return [];
+  }
+};
+const Todo = () => {
+  // stat variable
+  const [inputdata, setInputData] = useState("");
+  const [items, setItems] = useState(getLocalData());
+  const [isEditItem, setIsEditItem] = useState("");
+  const [togalButton, settoggalButton] = useState(false);
+
+  const addItem = () => {
+    if (!inputdata) {
+      alert("Fill the data");
+    } else if (inputdata && togalButton) {
+      setItems(
+        items.map((curElem) => {
+          if (curElem.id === isEditItem) {
+            return { ...curElem, name: inputdata };
+          }
+          return curElem;
+        })
+      );
+      setInputData([]);
+      setIsEditItem(null);
+      settoggalButton(false);
+    } else {
+      const myNewInputData = {
+        id: new Date().getTime().toString(),
+        name: inputdata,
+      };
+      setItems([...items, myNewInputData]);
+      setInputData("");
+    }
+  };
+
+  const editItem = (index) => {
+    const item_todo_edited = items.find((curElem) => {
+      return curElem.id === index;
+    });
+    setInputData(item_todo_edited.name);
+    setIsEditItem(index);
+    settoggalButton(true);
+  };
+
+  const deleteItem = (index) => {
+    const updatedItem = items.filter((curElem) => {
+      return curElem.id !== index;
+    });
+    setItems(updatedItem);
+  };
+
+  const removeAll = () => {
+    setItems([]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("mytodolist", JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <img src="./image/todo.svg" alt="todologo" />
+            <figcaption></figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="✍️ Add Item"
+              className="form-control"
+              value={inputdata}
+              onChange={(event) => setInputData(event.target.value)}
+            />
+            {togalButton ? (
+              <i className="far fa-edit add-btn" onClick={addItem}></i>
+            ) : (
+              <i className="fa fa-plus add-btn" onClick={addItem}></i>
+            )}
+          </div>
+          <div className="showItems">
+            {items.map((curElem) => {
+              return (
+                <div className="eachItem" key={curElem.id}>
+                  <h3>{curElem.name}</h3>
+                  <div className="todo-btn">
+                    <i
+                      className="far fa-edit add-btn"
+                      onClick={() => editItem(curElem.id)}
+                    ></i>
+                    <i
+                      className="far fa-trash-alt add-btn"
+                      onClick={() => deleteItem(curElem.id)}
+                    ></i>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <button
+            className="btn effect04"
+            data-sm-link-text="Remove All"
+            onClick={removeAll}
+          >
+            <span>CHECK LIST</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
